69d57eebad9c251adde6d02f8e2ef5e7d1ed329c
tranvanmanh9325 <manhtrana1k45tl@gmail.com>
2025-08-13 19:06:39 +0700

    Sửa icon Next Week

--- src/components/tasks/SelectDatePopup.jsx
+++ src/components/tasks/SelectDatePopup.jsx
@@ -99,15 +99,14 @@ const SelectDatePopup = ({ selectedDate, onChange, onClose }) => {
           <span className="icon">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
-              fill="none"
+              width="24"
+              height="24"
+              fill="currentColor"
               viewBox="0 0 24 24"
               aria-hidden="true"
               focusable="false"
             >
               <path
-                fill="currentColor"
                 fillRule="evenodd"
                 d="M18 4H6a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2M5 6a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1zm8.354 4.647a.5.5 0 0 0-.708.707l1.647 1.647H8.5a.5.5 0 1 0 0 1h5.793l-1.647 1.646a.5.5 0 0 0 .708.707l2.5-2.5a.5.5 0 0 0 0-.707zM7 8a.5.5 0 0 0 0 1h10a.5.5 0 0 0 0-1z"
                 clipRule="evenodd"
