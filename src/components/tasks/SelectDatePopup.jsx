4f43759d1196393eea8517a13c9968db66afe9ec
tranvanmanh9325 <manhtrana1k45tl@gmail.com>
2025-08-13 19:37:34 +0700

    Thay icon nút This Weekend

--- src/components/tasks/SelectDatePopup.jsx
+++ src/components/tasks/SelectDatePopup.jsx
@@ -86,7 +86,21 @@ const SelectDatePopup = ({ selectedDate, onChange, onClose }) => {
             onClose();
           }}
         >
-          <span className="icon">🖥</span>
+          <span className="icon">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              aria-hidden="true"
+              focusable="false"
+            >
+              <path
+                fill="currentColor"
+                d="M16 6a3 3 0 0 1 3 3v1h.1c1 0 1.9 1 1.9 2v4c0 1-.8 2-1.9 2H18v.5a.5.5 0 0 1-1 0V18H7v.5a.5.5 0 0 1-1 0V18H5a2 2 0 0 1-2-2v-4c0-1.1.9-2 2-2V9a3 3 0 0 1 3-3zm3 5a1 1 0 0 0-1 .9V15H6v-3a1 1 0 0 0-2-.1V16c0 .5.4 1 .9 1H19a1 1 0 0 0 1-.9V12c0-.6-.4-1-1-1m-3-4H8c-1 0-2 .8-2 1.9v1.4c.6.3 1 1 1 1.7v2h10v-2a2 2 0 0 1 1-1.7V9c0-1-.8-2-1.9-2z"
+              ></path>
+            </svg>
+          </span>
           <span className="label">This weekend</span>
         </div>
         <div
